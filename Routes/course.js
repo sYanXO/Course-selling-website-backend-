2349bdc7f83f8d81c6d508cb694c611f894850df
sreayan <sreayan69@gmail.com>
2024-10-05 10:39:18 +0530

    added good rutes using router

---
+++ Routes/course.js
@@ -0,0 +1,14 @@
+const {Router} = require("express");
+
+const courseRouter = Router();
+courseRouter.get("/", function(req,res){
+    
+})
+
+courseRouter.post("/purchase", function(req,res){
+    
+})
+
+module.exports = {
+    courseRouter:courseRouter
+}
