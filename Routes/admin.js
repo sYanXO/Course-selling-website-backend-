8d67f708a6a3821a62a26c250e7035bea2ad23cc
sreayan <sreayan69@gmail.com>
2024-10-06 21:22:15 +0530

    added admin routes

---
+++ Routes/admin.js
@@ -0,0 +1,31 @@
+const {Router} = require("express");
+
+const adminRouter = Router();
+
+
+adminRouterRouter.post("/signup", function(req,res){
+
+})
+
+adminRouter.post("/signin", function(req,res){
+    
+})
+
+
+adminRouter.post("/course",function (req,res){
+
+})
+
+adminRouter.put("/course",function (req,res){
+
+})
+
+adminRouter.get("/course",function (req,res){
+
+})
+
+module.exports ={
+    adminRouter:adminRouter
+}
+
+
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const {userRouter} = require("./Routes/user");
+const {courseRouter} = require("./Routes/course");
+const {adminRouter} = require("./Routes/admin");
 const app = express();
 
 app.use("/api/v1/user",userRouter);
+app.use("/api/v1/admin",adminRouter);
 app.use("/api/v1/course",courseRouter);
 
 
