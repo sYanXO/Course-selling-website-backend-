faa5fecafeecce5b0066b509f89b9836d4965923
sreayan <sreayan69@gmail.com>
2024-10-06 22:24:16 +0530

    better

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const {userRouter} = require("./Routes/user");
 const {courseRouter} = require("./Routes/course");
 const {adminRouter} = require("./Routes/admin");
@@ -8,5 +9,10 @@ app.use("/api/v1/user",userRouter);
 app.use("/api/v1/admin",adminRouter);
 app.use("/api/v1/course",courseRouter);
 
-
-app.listen(3000);
+async function main(){
+    // .env usage here :\
+    await mongoose.connect("mongodb+srv://sYan:qTAYM3EaEM9qwdi8@cluster0.j59zvyi.mongodb.net/coursera-App");
+    app.listen(3000);
+    console.log("listening @ 3000");
+}
+main();
